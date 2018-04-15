19ef211fbf2847252e1d902486396ab82e3c36dd
GaneshV757 <ganeshv757@gmail.com>
2018-04-15 12:50:34 -0700

    trying background listenting

--- recognition.py
+++ recognition.py
@@ -1,11 +1,24 @@
+import time
 import speech_recognition as sr
 
-r = sr.Recognizer()
+m = sr.Microphone()
+
+def calibrate(r):
+	with m as source:
+		r.adjust_for_ambient_noise(source)
+
+def initialize_recognizer():
+	r = sr.Recognizer()
+	calibrate(r)
+	return r
+
+r = initialize_recognizer()
 
 def get_voice_command():
 	# obtain audio from the microphone
 	with sr.Microphone() as source:
 		print("Listening...")
+		r.adjust_for_ambient_noise(source)
 		audio = r.listen(source)
 
 	# recognize speech using Sphinx
@@ -20,4 +33,20 @@ def get_voice_command():
 
 	return output
 
-get_voice_command()
+# get_voice_command()
+
+def callback(r, audio):
+	output = None
+	try:
+		output = r.recognize_sphinx(audio)
+		print("Sphinx thinks you said " + output)
+	except sr.UnknownValueError:
+		print("Sphinx could not understand audio")
+	except sr.RequestError as e:
+		print("Sphinx error; {0}".format(e))
+
+stop_listening = r.listen_in_background(m, callback)
+
+for _ in range(150): time.sleep(0.1)
+
+stop_listening(wait_for_stop=False)
